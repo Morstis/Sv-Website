742aa288700b6be3998a034c69b801ffcb0ee5ab
Mors <lucas.wiese@gmx.de>
2019-06-03 21:19:01 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Sv Website'; //alte Version
-  //mit neuem Titel
+  title = 'Sv Website';
 }
