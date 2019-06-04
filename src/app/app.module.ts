a2abfe770281f8deeed495d48eb888a7814e7667
Mors <lucas.wiese@gmx.de>
2019-06-04 22:36:18 +0200

    waypoint dynamisch gemacht(grade Anzahl)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_template/header/header.component';
+import { WaypointComponent } from './_template/waypoint/waypoint.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, WaypointComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
