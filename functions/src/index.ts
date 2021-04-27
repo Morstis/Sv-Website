247711959c65cba366896edda6ba6c101eb74e4c
Lucas Wiese <lucas.wiese@gmx.de>
2021-04-27 12:39:07 +0200

    inital | ssr + pwa conf

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions
+  .region('europe-west1')
+  .https.onRequest((request, response) => {
+    functions.logger.info('Hello logs!', { structuredData: true });
+    response.send('Hello from Firebase!');
+  });
+
+const expressApp = require('../dist/Sv-Website3/server/main').app();
+
+exports.ssr = functions
+  .region('us-central1')
+  .runWith({})
+  .https.onRequest(expressApp);
